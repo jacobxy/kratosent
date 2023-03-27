7444de056d305f0bdaf83492f62630786d476218
lok.lee <lok.lee@timeresearch.com>
2023-03-27 18:54:18 +0800

    x

---
+++ docker-compose/mysql/apps/mysql/init/test.sql
@@ -0,0 +1,17 @@
+-- CREATE DATABASE IF NOT EXISTS test default charset utf8;
+
+-- use test;
+
+-- DROP TABLE IF EXISTS `test_table`;
+
+-- CREATE TABLE `test_table` (
+--     `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+--     `name` varchar(20) DEFAULT NULL COMMENT '姓名',
+--     `sex` varchar(11) DEFAULT NULL COMMENT '性别',
+--     PRIMARY KEY(`id`)
+-- )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- INSERT INTO `test_table` (`id`,`name`,`sex`) 
+-- VALUES
+-- (1,'张三','男'),
+-- (2,'李四','男')
